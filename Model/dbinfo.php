816d2a84930b3c079b0ce91cb60f8fcf21f893c5
THOMPSON <you@example.com>
2022-10-24 08:13:40 -0500

    [sql stament to get all items alphabeticly]

--- Model/dbinfo.php
+++ Model/dbinfo.php
@@ -1,21 +1,24 @@
 <?php
-function getSpoopyItems(){
-   $dsn = "mysql:host=localhost;dbname=halloweenshop";
-   $username = "root";
-   $password = "";
-   try {
-       $db = new PDO($dsn, $username, $password);
-       //echo("connected");
-   } catch (PDOException $e){
-       //die(include "404.php");
-       echo("Not connected");
-        
-   }
 
-   $myQuery = "Select * from goodies";
-   global $db;
-   $qry = $db->query($myQuery);       
-   $goodies = $qry->fetchAll();
-   return $goodies;
-}
+    $dsn = "mysql:host=localhost;dbname=halloweenshop";
+    $username = "root";
+    $password = "";
+    try {
+        $db = new PDO($dsn, $username, $password);
+        //echo("connected");
+    } catch (PDOException $e){
+        //die(include "404.php");
+        echo("Not connected");
+        
+    }
+
+    function getSpoopyItems()
+    {
+        $myQuery = "select all * FROM goodies order by item ASC;";
+        global $db;
+        $qry = $db->query($myQuery);       
+        $goodies = $qry->fetchAll();
+        return $goodies;
+    }
+
 ?>
