0fa834607e9089fe9515b938de43e901a75f9a3a
ChauntelAC <cmacully@gmail.com>
2022-10-26 08:57:28 -0500

    updated cards

--- view/witchSoup.php
+++ view/witchSoup.php
@@ -7,21 +7,25 @@
     
 ?>
 
-<form action="." method="post">
-    <label>Item: <?php echo($itemName);?></label>
-    <label>Price: $<?php echo($itemPrice);?></label>
-    <label>Description: <?php echo($itemDescription);?></label>
-    <?php echo("<img src='images/$itemImg' alt=''>")?>
-
-    <label>Quantity:</label>
-    <select name="itemqty">
-    <?php for($i = 1; $i <= 10; $i++) : ?>
-        <option value="<?php echo $i; ?>">
-            <?php echo $i; ?>
-        </option>
-    <?php endfor; ?>
-    </select><br>
-    <?php echo(" <input type='hidden' value='$itemID' name='id'>") ?>
-    <input type="hidden" value="add" name="action">
-    <input type="submit" value="Add Item">
+<form style="max-width:600px; margin: 0 auto; padding-top:25px;" action="." method="post">
+    <div class='card' style='width: 450px; margin: 0 auto;'>
+        <div class='card-body'>
+            <?php echo("<img src='images/$itemImg' alt='' class='d-grid mx-auto'>")?><br>
+            <p class='card-title' style='display: flex; justify-content: center;'>Spoopy Item: <?php echo($itemName);?></p>
+            <p class='card-text' style='display: flex; justify-content: center;'>Price: $<?php echo($itemPrice);?></p>
+            <p class='card-text' style='display: flex; justify-content: center;'>Description: <?php echo($itemDescription);?></p>
+            <label style='display: flex; justify-content: center;'>Quantity: &emsp;
+                <select name="itemqty">
+                    <?php for($i = 1; $i <= 10; $i++) : ?>
+                    <option value="<?php echo $i; ?>">
+                    <?php echo $i; ?>
+                    </option>
+                    <?php endfor; ?>
+                </select>
+            </label><br>
+            <?php echo(" <input type='hidden' value='$itemID' name='id'>") ?>
+            <input type="hidden" value="add" name="action">
+            <input type="submit" class="buttonColor d-grid gap-2 col-3 mx-auto" value="Add Item">
+        </div>
+    </div>
 </form>
