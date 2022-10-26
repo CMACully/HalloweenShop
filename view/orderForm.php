f170a84e28485506f9c140174456c5b765247a5b
ChandlerThompson <chandlerthompson2001@gmail.com>
2022-10-26 09:40:24 -0500

    added a break

--- view/orderForm.php
+++ view/orderForm.php
@@ -1,11 +1,15 @@
 <form action="#" method="post">
     <input type="hidden" name="action" value="add">
     <label>Click on an item to view it:</label>
-   
+   <br>
     <?php 
         foreach($goodies as $goodie)
         {
-            echo("<input type='submit' value='${goodie[1]}' name='action'>");
+            
+            echo("<input type='submit' value='${goodie[1]}' name='action'><br>");
+            
+              
+            
            
         }
     ?>
